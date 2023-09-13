bf6bc8f48942a1553df58166420482c5b2470f78
okpc579 <ski579579@naver.com>
2023-09-13 01:46:48 +0000

    kpaas word change

--- src/postgres/download_postges_11.xx.sh
+++ src/postgres/download_postges_11.xx.sh
@@ -1,3 +1,4 @@
 wget https://ftp.postgresql.org/pub/source/v11.15/postgresql-11.15.tar.gz
 wget https://ftp.postgresql.org/pub/source/v11.16/postgresql-11.16.tar.gz
 wget https://ftp.postgresql.org/pub/source/v11.17/postgresql-11.17.tar.gz
+wget https://ftp.postgresql.org/pub/source/v11.21/postgresql-11.21.tar.gz
